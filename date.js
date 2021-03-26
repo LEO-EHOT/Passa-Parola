ae7bca909fdbfc6cac22a0a94a230d07f5c1d2c6
live-moment <62284799+live-moment@users.noreply.github.com>
2021-03-26 10:59:08 +0900

    Update date.js

--- date.js
+++ date.js
@@ -4,9 +4,9 @@ const dateContainer = document.querySelector(".js-date")
 function getDate(){
     const date = new Date();
     const year = date.getFullYear();
-    const month = date.getMonth();
+    const month = date.getMonth()+1;
     const day = date.getDate();
-    dateContainer.innerText = `${year}.${month < 10 ? `0${month+1}` : month+1}.${day < 10 ? `0${day}` : day}`
+    dateContainer.innerText = `${year}.${month < 10 ? `0${month}` : month}.${day < 10 ? `0${day}` : day}`
 }
 
 function init(){
